f9c17550eab27dca109ff281f58f2c92df62d428
EmanuelScherer <emanuel.scherer@meta.com.br>
2022-08-30 18:21:13 -0300

    remove user table

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 100;
+$plugin->version = 101;
 $plugin->component = 'local_klaza';
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
 $plugin->maturity = MATURITY_STABLE;
 
 ?>
